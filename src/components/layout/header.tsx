8d17134bf5f8ff7f0c11caab1e6ef9703e097c28
John Serrano <andrey_201015@hotmail.com>
2025-11-22 10:37:54 -0500

    Changes styles header and nav

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -5,16 +5,32 @@ export function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.container}>
-        <div className={styles.logo}>RecipeApp</div>
+        <div className={styles.logo}>
+          Recipe <span className={styles['text-app']}>App</span>
+        </div>
         <nav className={styles.nav}>
           <ul>
-            <li><Link href="/">Home</Link></li>
-            <li><Link href="/vegetarianos">Vegetarianos</Link></li>
-            <li><Link href="/platos-principales">Platos Principales</Link></li>
-            <li><Link href="/tortas">Tortas</Link></li>
-            <li><Link href="/comida-rapida">Comida Rápida</Link></li>
-            <li><Link href="/menu-ninos">Menú Niños</Link></li>
-            <li><Link href="/sopas">Sopas</Link></li>
+            <li>
+              <Link href='/'>Home</Link>
+            </li>
+            <li>
+              <Link href='/vegetarianos'>Vegetarianos</Link>
+            </li>
+            <li>
+              <Link href='/platos-principales'>Platos Principales</Link>
+            </li>
+            <li>
+              <Link href='/tortas'>Tortas</Link>
+            </li>
+            <li>
+              <Link href='/comida-rapida'>Comida Rápida</Link>
+            </li>
+            <li>
+              <Link href='/menu-ninos'>Menú Niños</Link>
+            </li>
+            <li>
+              <Link href='/sopas'>Sopas</Link>
+            </li>
           </ul>
         </nav>
       </div>
