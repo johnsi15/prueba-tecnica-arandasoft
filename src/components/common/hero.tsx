ad45d3f6f4078c06d31e4cdc26b3de610ec24875
John Serrano <andrey_201015@hotmail.com>
2025-11-23 15:57:43 -0500

    Refactor Hero component to use Link for category navigation and update styles

--- src/components/common/hero.tsx
+++ src/components/common/hero.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from './hero.module.scss'
 
 export function Hero() {
@@ -19,28 +20,40 @@ export function Hero() {
 
       <ul className={styles.categories}>
         <li>
-          <Image src='/assets/ic_vegetarianos.svg' alt='Vegetarianos' width={50} height={50} />
-          <span>Vegetarianos</span>
+          <Link href='/vegetarianos'>
+            <Image src='/assets/ic_vegetarianos.svg' alt='Vegetarianos' width={50} height={50} />
+            <span>Vegetarianos</span>
+          </Link>
         </li>
         <li>
-          <Image src='/assets/ic_principales.svg' alt='Platos Principales' width={50} height={50} />
-          <span>Principales</span>
+          <Link href='/platos-principales'>
+            <Image src='/assets/ic_principales.svg' alt='Platos Principales' width={50} height={50} />
+            <span>Principales</span>
+          </Link>
         </li>
         <li>
-          <Image src='/assets/ic_tortas.svg' alt='Tortas' width={50} height={50} />
-          <span>Tortas</span>
+          <Link href='/tortas'>
+            <Image src='/assets/ic_tortas.svg' alt='Tortas' width={50} height={50} />
+            <span>Tortas</span>
+          </Link>
         </li>
         <li>
-          <Image src='/assets/ic_rapida.svg' alt='Comida Rápida' width={50} height={50} />
-          <span>Rápida</span>
+          <Link href='/comida-rapida'>
+            <Image src='/assets/ic_rapida.svg' alt='Comida Rápida' width={50} height={50} />
+            <span>Rápida</span>
+          </Link>
         </li>
         <li>
-          <Image src='/assets/ic_menu_ninos.svg' alt='Menú Niños' width={50} height={50} />
-          <span>Menú Niños</span>
+          <Link href='/menu-ninos'>
+            <Image src='/assets/ic_menu_ninos.svg' alt='Menú Niños' width={50} height={50} />
+            <span>Menú Niños</span>
+          </Link>
         </li>
         <li>
-          <Image src='/assets/ic_sopas.svg' alt='Sopas' width={50} height={50} />
-          <span>Sopas</span>
+          <Link href='/sopas'>
+            <Image src='/assets/ic_sopas.svg' alt='Sopas' width={50} height={50} />
+            <span>Sopas</span>
+          </Link>
         </li>
       </ul>
     </section>
